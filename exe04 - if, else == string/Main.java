af520612a813ab17625a91c30f6096da0a8a32e8
Leandro Pagano <leandro.pagano@hotmail.com>
2025-09-21 23:05:54 -0300

    exe04
    obs: para comparar uma String o correto é utilizar o equals e não ==.

---
+++ exe04 - if, else == string/Main.java
@@ -0,0 +1,13 @@
+//exercicio java - fixação utilizando equals para comparar == string
+
+public class Main {
+
+    public static void main(String[] args){
+        String str = "Pagano";
+
+        if(str == "Pagano"){
+            System.out.println("Verdadeiro");
+        }else System.out.println("False");
+
+    }
+}
